cc7a3ab93f119f3daf7d1dee186ccb91e5f52c3d
sebek <sebek@dell>
2016-10-25 23:05:42 +0200

    nauka

---
+++ src/nauka/Nauka.java
@@ -0,0 +1,8 @@
+package nauka;
+public class Nauka {
+    public static void main(String[] args) {
+            int a = 5; a++;
+                System.out.println(a);
+                
+            }
+        }
